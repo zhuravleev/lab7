68eb0c2401a9420597499758f0fe8b7d6f7cae04
Zerstörer <yuriyzhuravlev171@gmail.com>
2020-12-14 16:52:28 +0700

    First

---
+++ 1/script.js
@@ -0,0 +1,17 @@
+let a = 5; 
+let str = "5"; 
+let b = null; 
+let yes = true; 
+
+console.log(+b)
+// Null v int
+console.log(+str)
+//str v int
+console.log(+yes)
+// boolean v int
+console.log(`${yes}`)
+// boolean v str
+console.log(`${b}`)
+// null v str
+console.log(`${a}`)
+// int v str
