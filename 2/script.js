f0a3ae2107d1d15013438e8da88ccc44cb780afb
Zerstörer <yuriyzhuravlev171@gmail.com>
2020-12-14 17:08:23 +0700

    Second

---
+++ 2/script.js
@@ -0,0 +1,10 @@
+let a = +prompt("a=");
+let b = +prompt("b=");
+
+let S = a*b; 
+let P = (a + b)*2
+
+console.log(`a = ${a} b = ${b}`)
+console.log(`S = ${S}`);
+console.log(`P = ${P}`);
+console.log(`P/S = ${P/S}`);
